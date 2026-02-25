29e15a1c3f07388b3ecaaf9da53f09912e25ac9d
Rohan Rai <92577040+Rohanrai540@users.noreply.github.com>
2026-02-25 17:33:00 +0530

    created a submission

--- script.js
+++ script.js
@@ -1,6 +1,8 @@
 // complete the given function
 
 function palindrome(str){
-
+  let cleaned = s.toLowerCase().replace(/[^a-z0-9]/g, "");
+  let reversed = cleaned.split("").reverse().join("");
+  return cleaned === reversed;
 }
 module.exports = palindrome
